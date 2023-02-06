702961e8fc7d000c41680a9bd9a37d26db1a6ba0
Antonio Jorge Garcia de Noronha Junior <103082591+JuniorGarciaDev@users.noreply.github.com>
2023-02-06 09:39:35 -0300

    aprendendo a mexer com arquivos

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Ola, Mundo!");
+    public static void main(String[] args) {
+        String primeiroNome = "Antonio";
+        String segundoNome = "Jorge";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome , String segundoNome ) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
