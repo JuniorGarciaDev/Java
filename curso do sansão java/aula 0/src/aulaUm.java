5a94f6fa6513a8c3789fd83f14c15965d7e5ca4d
Antonio Jorge Garcia de Noronha Junior <103082591+JuniorGarciaDev@users.noreply.github.com>
2023-02-12 14:33:34 -0300

    aula zero e um criada

---
+++ curso do sansão java/aula 0/src/aulaUm.java
@@ -0,0 +1,7 @@
+public class aulaUm {
+
+    public static void main (String[] args) throws Exception{
+        System.out.println("teste");
+    }
+    
+}
---
+++ curso do sansão java/aula 0/src/aulaZero.java
@@ -0,0 +1,9 @@
+public class aulaZero {
+    public static void main(String[] args) throws Exception {
+        String nomeDoUsuario = "Junior";
+        int idadeDoUsuario = 19;
+        System.out.println("o nome do Usuario é " + nomeDoUsuario + " e a idade é de " + idadeDoUsuario + " Anos");
+        
+    
+    }
+}
